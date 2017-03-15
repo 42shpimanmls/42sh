b4c9be4c9f3fd4d51522beac4702bf6d248f936d
n0iz <n0iz@n0iz.ddns.net>
2017-03-15 15:18:13 +0100

    lexer

--- srcs/break_input/tokenizer/token.c
+++ srcs/break_input/tokenizer/token.c
@@ -1,14 +1,16 @@
 #include "utils.h"
 #include "token.h"
 #include <libft.h>
+#include "break_input/lexer.h"
 
-t_token	*construct_token(t_token_id id, char const *str, char delim)
+t_token	*construct_token(char const *str, char delim)
 {
 	t_token	*token;
 
 	token = memalloc_or_die(sizeof(t_token));
-	token->type = get_token_def(id);
+	token->type = get_token_def(TOKEN_TOKID);
 	token->str = ft_strdup(str);
 	token->delimiter = delim;
+	lex_me(token);
 	return (token);
 }
