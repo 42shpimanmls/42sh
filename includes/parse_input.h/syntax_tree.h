004cfc21c299a4c9c3c2a62c5cc78aa99f5c9e28
n0iz <n0iz@n0iz.ddns.net>
2017-03-17 00:16:39 +0100

    syntax_tree header

---
+++ includes/parse_input.h/syntax_tree.h
@@ -0,0 +1,37 @@
+#ifndef SYNTAX_TREE_H
+# define SYNTAX_TREE_H
+
+#include "init/init.h"
+#include "uint.h"
+
+typedef struct		s_redirection
+{
+	t_uint				n;
+	t_token_id			redir_op;
+	char const			*word;
+}					t_redirection;
+
+typedef struct		s_simple_command
+{
+	t_variable			*assignments;
+	t_redirection		**redirections;
+	char *const			*argv;
+}					t_simple_command;
+
+typedef struct		s_and_or
+{
+	t_simple_command	**pipeline;
+	t_token_id			separation_type;
+}					t_and_or;
+
+typedef struct		s_and_or_list
+{
+	s_and_or			**childs;
+}					t_and_or_list;
+
+typedef struct		s_command_list
+{
+	t_and_or_list		**childs;
+}					t_command_list;
+
+#endif
