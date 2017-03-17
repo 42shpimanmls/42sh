336a1028f987b6e540075b0f4972d46514672ee6
n0iz <n0iz@n0iz.ddns.net>
2017-03-17 18:50:02 +0100

    syntax tree uses lists now

--- includes/parse_input/syntax_tree.h
+++ includes/parse_input/syntax_tree.h
@@ -6,32 +6,31 @@
 
 typedef struct		s_redirection
 {
-	t_uint				n;
-	t_token_id			redir_op;
-	char const			*word;
+	struct s_redirection	*next;
+	t_uint					n;
+	t_token_id				redir_op;
+	char const				*word;
 }					t_redirection;
 
 typedef struct		s_simple_command
 {
-	t_variable			*assignments;
-	t_redirection		**redirections;
-	char *const			*argv;
+	struct s_simple_command	*next;
+	t_variable				*assignments;
+	t_redirection			*redirections;
+	char *const				*argv;
 }					t_simple_command;
-
-typedef struct		s_and_or
-{
-	t_simple_command	**pipeline;
-	t_token_id			separation_type;
-}					t_and_or;
 
 typedef struct		s_and_or_list
 {
-	s_and_or			**childs;
+	struct s_and_or_list	*next;
+	t_simple_command		*pipeline;
+	t_token_id				separation_type;
 }					t_and_or_list;
 
 typedef struct		s_command_list
 {
-	t_and_or_list		**childs;
+	struct s_command_list	*next;
+	t_and_or_list			*and_or_list;
 }					t_command_list;
 
 #endif
