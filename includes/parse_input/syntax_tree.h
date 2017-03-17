6a345a8156f6204b7d5a11832d02e9f17bca4493
n0iz <n0iz@n0iz.ddns.net>
2017-03-17 19:19:41 +0100

    more explicit enums

--- includes/parse_input/syntax_tree.h
+++ includes/parse_input/syntax_tree.h
@@ -1,14 +1,28 @@
 #ifndef SYNTAX_TREE_H
 # define SYNTAX_TREE_H
 
-#include "shell_env.h"
-#include "uint.h"
+# include "shell_env.h"
+# include "uint.h"
+
+typedef enum	e_ao_type
+{
+	AO_OR = OR_IF_TOKID,
+	AO_AND = AND_IF_TOKID,
+	AO_END = TOKEN_TOKID
+}				t_ao_type;
+
+typedef enum	e_redir_type
+{
+	REDIR_OUTPUT = GREAT_TOKID,
+	REDIR_INPUT = LESS_TOKID,
+	APPEND_OUTPUT = DGREAT_TOKID
+}				t_redir_type;
 
 typedef struct		s_redirection
 {
 	struct s_redirection	*next;
 	t_uint					n;
-	t_token_id				redir_op;
+	t_redir_type			type;
 	char const				*word;
 }					t_redirection;
 
@@ -24,7 +38,7 @@ typedef struct		s_and_or_list
 {
 	struct s_and_or_list	*next;
 	t_simple_command		*pipeline;
-	t_token_id				separation_type;
+	t_ao_type				separation_type;
 }					t_and_or_list;
 
 typedef struct		s_command_list
