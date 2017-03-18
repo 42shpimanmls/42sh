97edd1666f67ad819e0a99f262392c2957e2b17a
n0iz <n0iz@n0iz.ddns.net>
2017-03-18 04:25:02 +0100

    cleaning

--- srcs/utils/list_get_last_const.c
+++ srcs/utils/list_get_last_const.c
@@ -1,7 +1,8 @@
 #include "abstract_list.h"
 
-void	list_goto_last(t_abstract_list **list)
+t_abstract_list const	*list_get_last_const(t_abstract_list const *list)
 {
-	while ((*list) && (*list)->next)
-		(*list) = (*list)->next;
+	while (list && list->next)
+		list = list->next;
+	return (list);
 }
--- srcs/utils/list_goto_last.c
+++
@@ -1,8 +0,0 @@
-#include "abstract_list.h"
-
-t_abstract_list const	*list_get_last_const(t_abstract_list const *list)
-{
-	while (list && list->next)
-		list = list->next;
-	return (list);
-}
