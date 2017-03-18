f25f43d287bd34d2fe743a0dc5a2e2141f4f3b4a
n0iz <n0iz@n0iz.ddns.net>
2017-03-18 17:19:36 +0100

    fixed failing test

--- tests/srcs/main.c
+++ tests/srcs/main.c
@@ -12,7 +12,7 @@ static void test_maxi(void)
 {
 	CU_ASSERT(maxi(0,2) == 2);
 	CU_ASSERT(maxi(0,-2) == 0);
-	CU_ASSERT(maxi(2,2) == 0);
+	CU_ASSERT(maxi(2,2) == 2);
 }
 
 static void print_error(void)
