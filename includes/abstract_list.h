ae603d7a96957366102ab9c6d01109dea6c7a6d2
Moul <moul@moul.re>
2017-03-20 12:45:08 +0100

    [norm] includes/abstract_list.h.

--- includes/abstract_list.h
+++ includes/abstract_list.h
@@ -1,18 +1,17 @@
 #ifndef ABSTRACT_LIST_H
-#define ABSTRACT_LIST_H
+# define ABSTRACT_LIST_H
+# include <stdlib.h>
 
-#include <stdlib.h>
-
-typedef struct	s_abstract_list
+typedef struct				s_abstract_list
 {
 	struct s_abstract_list	*next;
-}				t_abstract_list;
+}							t_abstract_list;
 
-size_t					list_count(t_abstract_list *list);
-void					list_push_back(t_abstract_list **list_addr
-	, t_abstract_list *elem);
-t_abstract_list const	*list_get_last_const(t_abstract_list const *list);
-t_abstract_list			*list_dup_until(t_abstract_list const *list
-	, t_abstract_list const *stop, size_t elem_size);
+size_t						list_count(t_abstract_list *list);
+void						list_push_back(t_abstract_list **list_addr,
+t_abstract_list *elem);
+t_abstract_list const		*list_get_last_const(t_abstract_list const *list);
+t_abstract_list				*list_dup_until(t_abstract_list const *list,
+t_abstract_list const *stop, size_t elem_size);
 
 #endif
