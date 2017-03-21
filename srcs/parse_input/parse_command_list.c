9e0f1948cc6afc48ad5565d420b2de7336caf433
n0iz <n0iz@n0iz.ddns.net>
2017-03-21 13:53:37 +0100

    fix single semicolon behavior

--- srcs/parse_input/parse_command_list.c
+++ srcs/parse_input/parse_command_list.c
@@ -31,24 +31,29 @@ t_command_list	*parse_command_list(t_token const *tokens)
 		return (NULL);
 	trimed = trim_newlines(tokens);
 	splited = split_tokens_at(trimed, SEMI_TOKID, &splited_len);
-	if (last_is_semi(trimed))
-		splited_len--;
-	delete_all_tokens(&trimed);
-	u = 0;
 	result = NULL;
-	it = &result;
-	while (u < splited_len)
+	if (splited_len > 1 || splited[0] != NULL)
 	{
-		if (splited[u] == NULL)
+		if (last_is_semi(trimed))
+			splited_len--;
+		u = 0;
+		it = &result;
+		while (u < splited_len)
 		{
-			ft_putendl_fd("42sh: syntax error near unexpected token ';'", 2);
-			exit(1);
+			ft_putnbr(u);
+			ft_putchar('\n');
+			if (splited[u] == NULL)
+			{
+				ft_putendl_fd("42sh: syntax error near unexpected token ';'", 2);
+				exit(1);
+			}
+			(*it) = memalloc_or_die(sizeof(t_command_list));
+			(*it)->and_or_list = parse_and_or_list(splited[u]);
+			it = &(*it)->next;
+			u++;
 		}
-		(*it) = memalloc_or_die(sizeof(t_command_list));
-		(*it)->and_or_list = parse_and_or_list(splited[u]);
-		it = &(*it)->next;
-		u++;
 	}
+	delete_all_tokens(&trimed);
 	delete_tokens_array(splited, splited_len);
 	return (result);
 }
