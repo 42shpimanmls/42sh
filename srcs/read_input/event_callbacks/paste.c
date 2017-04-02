57883a8b357bd3a49e5592831eab8f38710d75bc
Foo bar <unknow>
2017-04-02 20:35:11 +0200

    improve pasting but not perfect

--- srcs/read_input/event_callbacks/paste.c
+++ srcs/read_input/event_callbacks/paste.c
@@ -6,10 +6,12 @@ EV_CB_RET	event_paste(EV_CB_ARGS)
 
 	if (if_on_multiline(ed))
 		return ;
+	clear_line(ed);
 	i = -1;
 	if (ed->selected_string)
 	{
 		while (ed->selected_string[++i])
 			add_to_string(ed, ed->selected_string[i]);
 	}
+	put_line(ed);
 }
