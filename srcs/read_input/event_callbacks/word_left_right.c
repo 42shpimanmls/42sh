5679f0f0f82afc492fef9a1fceb74298cc021044
Pierre LESPINASSE <pilespin@e3r7p6.42.fr>
2017-04-03 17:47:34 +0200

    file restructuration

--- srcs/read_input/event_callbacks/word_left_right.c
+++ srcs/read_input/event_callbacks/word_left_right.c
@@ -1,24 +1,6 @@
 #include "event_callback_def.h"
 #include <libft.h>
 
-EV_CB_RET 	event_cursor_home(EV_CB_ARGS)
-{
-	t_vec2i		cursor_vec;
-	t_vec2i		mov_vec;
-
-	cursor_vec = get_cursor_vector(ed);
-	ed->cursor_position = 0;
-	mov_vec = vec2i_sub(cursor_vec, get_cursor_vector(ed));
-	move_cursor(mov_vec, ed->term);
-}
-
-EV_CB_RET 	event_cursor_end(EV_CB_ARGS)
-{
-	clear_line(ed);
-	ed->cursor_position = ed->string_size;
-	put_line(ed);
-}
-
 static bool		is_white(char c)
 {
 	if (!c)
