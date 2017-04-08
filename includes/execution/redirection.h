afebe5d8b04188ebee8105542510880f5c6d8837
Foo bar <unknow>
2017-04-08 16:28:32 +0200

    Fix file permission with mask

--- includes/execution/redirection.h
+++ includes/execution/redirection.h
@@ -1,6 +1,12 @@
 #include "execute_syntax_tree.h"
 #include <libft.h>
 
+/*
+** FILE_PERMISSION same as 644
+*/
+
+#define FILE_PERMISSION (S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)
+
 int 			*save_stdin_stdout();
 t_error_id		redirect(t_redirection *redirections);
 void			restore_stdin_stdout(int *std);
