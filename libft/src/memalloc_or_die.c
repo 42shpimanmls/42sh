3f04fa776fb2cf055a4e42edfbc255c54337a0a2
Foo bar <unknow>
2017-04-09 22:22:54 +0200

    typo memalloc or die, It work

--- libft/src/memalloc_or_die.c
+++ libft/src/memalloc_or_die.c
@@ -7,7 +7,7 @@ void	*memalloc_or_die(size_t size)
 	m = malloc(size);
 	if (!m)
 	{
-		ft_printf("Oh no! malloc is too heavy for me");
+		ft_printf("Oh no! malloc is too heavy for me\n");
 		exit(-1);
 	}
 	ft_bzero(m, size);
