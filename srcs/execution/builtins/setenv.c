d3fb8d72df650fbf70cfd3bb8f26d1d2630ceafb
Pierre LESPINASSE <pilespin@e3r6p6.42.fr>
2017-04-10 19:34:55 +0200

    setenv with overwrite

--- srcs/execution/builtins/setenv.c
+++ srcs/execution/builtins/setenv.c
@@ -1,11 +1,12 @@
 # include "builtin_def.h"
 # include <libft.h>
+# include <signal.h>
 # include "shell_env.h"
 # include "abstract_list.h"
 # include "init/init.h"
 
-# define FORBIDDEN_CHAR "Is forbidden to use the character '='\n"
-# define USAGE "Usage: setenv <key> <value>\n"
+# define FORBIDDEN_CHAR	"Is forbidden to use the character '='\n"
+# define USAGE 			"Usage: setenv <key> <value>\n"
 
 BUILTIN_RET		builtin_setenv(BUILTIN_ARGS)
 {
@@ -20,8 +21,8 @@ BUILTIN_RET		builtin_setenv(BUILTIN_ARGS)
 			ft_dprintf(STDERR_FILENO, FORBIDDEN_CHAR);
 			return (STATUS_FAILURE);
 		}
-		setenv_as(env, argv[1], argv[2]);
-		return (STATUS_SUCCESS);
+		if (setenv_as(env, argv[1], argv[2]) == STATUS_SUCCESS)
+			return (STATUS_SUCCESS);
 	}
 	else
 	{
@@ -34,12 +35,15 @@ int			setenv_as(t_variable **env, char *name, char*value)
 {
 	if (env && ft_strlen(name) > 0)
 	{
-		if (check_if_variable_exist(*env, name))
+		if (variable_exist(*env, name))
 		{
-			unsetenv_as(env, name);
+			if (variable_is_overwritable(*env, name))
+				unsetenv_as(env, name);
+			else
+				return (STATUS_SUCCESS);
 		}
 		list_push_back((t_abstract_list**)&env,
-			(t_abstract_list*)create_variable(name, value, false));
+			(t_abstract_list*)create_variable(name, value, true, true));
 		return (STATUS_SUCCESS);
 	}
 	return (STATUS_FAILURE);
