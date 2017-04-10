a996471317fbad2059ac30ec185be8b583fcbb9c
n0iz <n0iz@n0iz.ddns.net>
2017-04-10 22:38:25 +0200

    use set_last_exit_status in wait_for_childs

--- srcs/utils/wait_for_childs.c
+++ srcs/utils/wait_for_childs.c
@@ -1,16 +1,26 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <stdlib.h>
+#include "uint.h"
 #include "errors.h"
+#include "utils.h"
 
 void				wait_for_childs()
 {
 	int		wstatus;
+	t_uchar	estatus;
 
 	set_error(NO_ERROR);
 	while(wait(&wstatus) != -1 && errno != ECHILD)
 	{
-		if (WIFEXITED(wstatus) && WEXITSTATUS(wstatus) != EXIT_SUCCESS)
-			set_error(CHILD_FAILURE);
+		if (WIFEXITED(wstatus))
+		{
+			estatus = WEXITSTATUS(wstatus);
+			set_last_exit_status(estatus);
+			if (estatus != EXIT_SUCCESS)
+				set_error(CHILD_FAILURE);
+		}
 	}
+	if (WIFEXITED(wstatus))
+		set_last_exit_status(WEXITSTATUS(wstatus));
 }
