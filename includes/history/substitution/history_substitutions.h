823f64e22172bea3bbc532576e1a60334411079c
Louise SIMONNEAU <lsimonne@e3r9p11.42.fr>
2017-04-25 19:48:16 +0200

    conflict fail

--- includes/history/substitution/history_substitutions.h
+++ includes/history/substitution/history_substitutions.h
@@ -57,7 +57,7 @@ char	*get_word_range(char *line, t_range *range);
 */
 
 bool	start_word_designator(char c);
-void	parse_word_designators(char *str, int *i, t_range *range, \
+char	*parse_word_designators(char *str, int *i, t_range *range, \
 										char **entry);
 
 /*
