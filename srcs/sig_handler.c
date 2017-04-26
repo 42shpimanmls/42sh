4d68037d66441bc9b7b69c8c4dcebb2ebd76e48a
Louise Simonneau <louise.simonneau@gmail.com>
2017-04-26 16:32:06 +0200

    [ctrlC] still a small bug but a bit better

--- srcs/sig_handler.c
+++ srcs/sig_handler.c
@@ -7,21 +7,13 @@
 void		sig_handler(int sig)
 {
 	t_editor	*ed;
-	t_shell_env	*shell_env;
 
 	ed = get_editor();
-	shell_env = get_shell_env();
 	if (sig == SIGINT)
 	{
-		if (shell_env->last_unmatched)
-		{
-			set_error(NO_ERROR);
-			shell_env->last_unmatched = 0;
-			ft_strdel(&shell_env->input_string);
-			clear_line(ed);
-			while (shell_env->should_run)
-				main_loop();
-		}
+		if (get_shell_env()->last_unmatched)
+			get_shell_env()->del_input = 1;
+		get_shell_env()->last_unmatched = NO_ERROR;
 		init_editor();
 		refresh_line(ed);
 		wait(0);
