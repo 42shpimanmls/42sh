e3a0fa98b9eda72f7e1806ba5f1fcdd50a2fcc48
n0iz <n0iz@n0iz.ddns.net>
2017-05-02 14:21:47 +0200

    fix exit for travis

--- srcs/execution/builtins/exit.c
+++ srcs/execution/builtins/exit.c
@@ -11,10 +11,14 @@ int		builtin_exit(int argc, char **argv)
 		ret = ft_atoi(argv[1]);
 	else
 		ret = get_last_exit_status();
-	if (ret < 0 || ret > 255)
+	if (ret < 0)
 	{
 		ft_dprintf(STDERR_FILENO, "%s: wrong exit value: %d\n", SHNAME, ret);
 		ret = 2;
+	}
+	else if (ret > 255)
+	{
+		ret = ret % 256;
 	}
 	exit(ret);
 }
