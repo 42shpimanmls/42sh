c55fdfbcd430f4d7348c966f43956f1221b7a4f2
Moul <moul@moul.re>
2017-05-05 11:52:48 +0200

    [norm] #26: 25 line per function: parse_command_list.c.

--- srcs/parse_input/parse_command_list.c
+++ srcs/parse_input/parse_command_list.c
@@ -19,13 +19,37 @@ static bool		last_is_semi(t_token const *tokens)
 		return (false);
 }
 
+static void	parse_cmd_list_sub(size_t splited_len, t_token **splited,
+	t_command_list *result, t_command_list  **it)
+{
+	size_t			u;
+
+	u = 0;
+	while (u < splited_len)
+	{
+		if (splited[u] == NULL)
+		{
+			set_error(UNEXPECTED_SEMI);
+			result = NULL; //delete_command_list(&result);
+			break ;
+		}
+		(*it) = memalloc_or_die(sizeof(t_command_list));
+		(*it)->and_or_list = parse_and_or_list(splited[u]);
+		if (get_error() != NO_ERROR)
+		{
+			result = NULL; //delete_command_list(&result);
+			break ;
+		}
+		it = &(*it)->next;
+		u++;
+	}
+}
+
 t_command_list	*parse_command_list(t_token const *tokens)
 {
 	t_command_list	*result;
-	t_command_list	**it;
 	t_token			**splited;
 	size_t			splited_len;
-	size_t			u;
 	t_token			*trimed;
 
 	set_error(NO_ERROR);
@@ -38,26 +62,7 @@ t_command_list	*parse_command_list(t_token const *tokens)
 	{
 		if (last_is_semi(trimed))
 			splited_len--;
-		u = 0;
-		it = &result;
-		while (u < splited_len)
-		{
-			if (splited[u] == NULL)
-			{
-				set_error(UNEXPECTED_SEMI);
-				result = NULL; //delete_command_list(&result);
-				break ;
-			}
-			(*it) = memalloc_or_die(sizeof(t_command_list));
-			(*it)->and_or_list = parse_and_or_list(splited[u]);
-			if (get_error() != NO_ERROR)
-			{
-				result = NULL; //delete_command_list(&result);
-				break ;
-			}
-			it = &(*it)->next;
-			u++;
-		}
+		parse_cmd_list_sub(splited_len, splited, result, &result);
 	}
 	delete_all_tokens(&trimed);
 	delete_tokens_array(splited, splited_len);
