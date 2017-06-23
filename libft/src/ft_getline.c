d3757ce329fb51e814ceb67aadd1cd7a72363cc0
Norman MEIER <nmeier@e1r8p18.42.fr>
2017-06-23 16:24:53 +0200

    fix getline return null on empty line

--- libft/src/ft_getline.c
+++ libft/src/ft_getline.c
@@ -6,7 +6,7 @@
 /*   By: pilespin <pilespin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/22 12:07:13 by pilespin          #+#    #+#             */
-/*   Updated: 2016/02/29 18:19:45 by pilespin         ###   ########.fr       */
+/*   Updated: 2017/06/23 16:23:35 by nmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static char	*ft_parse(char **stk, char *tmp, int fd)
 {
 	int	i;
+	char *otmp;
 
 	if (ft_strchr(tmp, '\n'))
 	{
@@ -24,7 +25,10 @@ static char	*ft_parse(char **stk, char *tmp, int fd)
 			if (tmp[i] == '\n')
 			{
 				stk[fd] = ft_strsub(tmp, i + 1, ft_strlen(tmp) - i - 1);
-				return (ft_strsub(tmp, 0, i));
+				otmp = ft_strsub(tmp, 0, i);
+				if (!otmp)
+					otmp = ft_strnew(0);
+				return (otmp);
 			}
 		}
 	}
@@ -58,6 +62,8 @@ char		*ft_getline(int const fd)
 			buff[ret] = '\0';
 			tmp = ft_strjoinf(tmp, buff, 1);
 			tmp2 = ft_parse(stk, tmp, fd);
+			if (tmp2 == NULL)
+				tmp2 = ft_strnew(0);
 			break ;
 		}
 		free(buff);
