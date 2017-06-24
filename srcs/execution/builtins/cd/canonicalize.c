ac8846885ddc2ea398fecb65283ddcb6c7ab2f33
Alaric SENAT <asenat@e1r10p23.42.fr>
2017-06-24 16:46:34 +0200

    cdnorming started

--- srcs/execution/builtins/cd/canonicalize.c
+++ srcs/execution/builtins/cd/canonicalize.c
@@ -1,89 +1,16 @@
-#include "execution/builtins/builtins.h"
-#include "variable.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <libft.h>
-#include "uint.h"
-#include "errors.h"
-#include <errno.h>
-#include "utils.h"
-#include "strlist.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   canonicalize.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asenat <asenat@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/23 17:49:16 by asenat            #+#    #+#             */
+/*   Updated: 2017/06/23 17:49:24 by asenat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static t_strlist	*break_components(char const *path)
-{
-	char const	*start;
-	t_strlist	*cmpnts;
-	char		*tmp;
-
-	start = path;
-	cmpnts = NULL;
-	while (start != NULL)
-	{
-		while (*path != '\0' && *path != '/')
-			path++;
-		while (*path != '\0' && *path == '/')
-			path++;
-		tmp = strdup_until(start, path);
-		strlist_append(&cmpnts, tmp);
-		free(tmp);
-		if (*path == '\0')
-			start = NULL;
-		else
-			start = path;
-	}
-	return (cmpnts);
-}
-
-static bool			comp_is_dot(char const *comp)
-{
-	if (ft_strnequ(comp, "./", 2) || ft_strequ(comp, "."))
-		return (true);
-	else
-		return (false);
-}
-
-static bool			comp_is_dot_dot(char const *comp)
-{
-	if (ft_strnequ(comp, "../", 3) || ft_strequ(comp, ".."))
-		return (true);
-	else
-		return (false);
-}
-
-static void			remove_dots(t_strlist **cmpnts_addr)
-{
-	t_strlist	*component;
-
-	component = *cmpnts_addr;
-	while (component != NULL)
-	{
-		if (comp_is_dot(component->str))
-		{
-			*cmpnts_addr = component->next;
-			component->next = NULL;
-			strlist_delete(&component);
-		}
-		else
-		{
-			cmpnts_addr = &component->next;
-		}
-		component = *cmpnts_addr;
-	}
-}
-
-static bool			comp_is_directory(t_strlist *start, t_strlist *end)
-{
-	char	*str;
-	bool	result;
-
-	str = strlist_to_str_until(start, end->next);
-	if (is_directory(str))
-		result = true;
-	else
-		result = false;
-	free(str);
-	return (result);
-}
+#include "execution/builtins/cd.h"
 
 static int			try_remove_dot_dots(t_strlist **cmpnts_addr)
 {
@@ -94,7 +21,8 @@ static int			try_remove_dot_dots(t_strlist **cmpnts_addr)
 	start = component;
 	while (component != NULL && component->next != NULL)
 	{
-		if (comp_is_dot_dot(component->next->str) && component->str[0] != '/' && !comp_is_dot_dot(component->str))
+		if (comp_is_dot_dot(component->next->str) &&
+				component->str[0] != '/' && !comp_is_dot_dot(component->str))
 		{
 			if (!comp_is_directory(start, component))
 				return (EXIT_FAILURE);
@@ -103,9 +31,7 @@ static int			try_remove_dot_dots(t_strlist **cmpnts_addr)
 			strlist_delete(&component);
 		}
 		else
-		{
 			cmpnts_addr = &component->next;
-		}
 		component = *cmpnts_addr;
 	}
 	return (EXIT_SUCCESS);
@@ -139,21 +65,17 @@ static void			simplify(t_strlist *components)
 }
 
 /*
-8 The curpath value shall then be converted to canonical form as follows, considering each component from beginning to end, in sequence:
-
-	a Dot components and any <slash> characters that separate them from the next component shall be deleted.
-
-	b For each dot-dot component, if there is a preceding component and it is neither root nor dot-dot, then:
-
-		i If the preceding component does not refer (in the context of pathname resolution with symbolic links followed) to a directory,
-			then the cd utility shall display an appropriate error message and no further steps shall be taken.
-
-		ii The preceding component, all <slash> characters separating the preceding component from dot-dot, dot-dot, and all <slash> characters
-			separating dot-dot from the following component (if any) shall be deleted.
-
-	c An implementation may further simplify curpath by removing any trailing <slash> characters that are not also leading <slash> characters,
-		replacing multiple non-leading consecutive <slash> characters with a single <slash>, and replacing three or more leading <slash> characters
-		with a single <slash>. If, as a result of this canonicalization, the curpath variable is null, no further steps shall be taken.
+** The curpath value shall then be converted to canonical form as follows, considering each component from beginning to end, in sequence:
+**a Dot components and any <slash> characters that separate them from the next component shall be deleted.
+**b For each dot-dot component, if there is a preceding component and it is neither root nor dot-dot, then:
+**i If the preceding component does not refer (in the context of pathname resolution with symbolic links followed) to a directory,
+**then the cd utility shall display an appropriate error message and no further steps shall be taken.
+**
+**ii The preceding component, all <slash> characters separating the preceding component from dot-dot, dot-dot, and all <slash> characters
+**separating dot-dot from the following component (if any) shall be deleted.
+**c An implementation may further simplify curpath by removing any trailing <slash> characters that are not also leading <slash> characters,
+**replacing multiple non-leading consecutive <slash> characters with a single <slash>, and replacing three or more leading <slash> characters
+**with a single <slash>. If, as a result of this canonicalization, the curpath variable is null, no further steps shall be taken.
 */
 
 char				*canonicalize_path(char const *path)
